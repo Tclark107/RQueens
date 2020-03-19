7b455e1671a5b259678cb75f1a8a015f8c4016bf
tclark107 <tcjc107@gmail.com>
2020-03-19 14:34:47 -0700

    cleaned up the comments

--- RQueens.java
+++ RQueens.java
@@ -1,31 +1,19 @@
 //------------------------------------------------------------------------------------------------------------------
 // RQueens.java
 // A recursive solution to the n-Queens problem.
+// Not meant for numbers bigger than 16, it will be too slow.
 // Tristan Clark
 //------------------------------------------------------------------------------------------------------------------
 
 class RQueens{
 
-    /*implements the recursive algorithm we have been describing. If the string argument mode has the value
+    // findSolutions(B,i,mode)
+    /* implements the recursive algorithm we have been describing. If the string argument mode has the value
     "verbose", then findSolutions() will print out solutions as it finds them by calling the function
     printBoard(). If mode has any other value, findSolutions() will print nothing. The int returned by
     findSolutions() is the number of solutions to n-queens that have queens placed on rows 1 to (i − 1),
-    as represented by the current state of array B[][]. A top level call to findSolutions() that does not
-    print solutions would be findSolutions(B, 1, ""). High level pseudo-code for this function is given
-    below.
-        1. if i > n (a queen was placed on row n, and hence a solution was found)
-        2.      if we are in verbose mode
-        3.          printBoard([][] B);
-        4.      return 1
-        5. else
-        6.      for each square on row i
-        7.          if that square is safe
-        8.              place a queen on that square
-        9.              recur on row (i + 1), then add the returned value to an accumulating sum
-        10.             remove the queen from that square
-        11. return the accumulated sum*/
+    as represented by the current state of array B[][]. */
     static int findSolutions(int[][] B, int i, String mode){
-        //printWholeBoard(B);
         int sum = 0;
         if(i == B.length) {
             if(mode == "-v") printBoard(B);
@@ -39,13 +27,12 @@ class RQueens{
                 }
             }
         }
-        //System.out.println("sum: "+ sum);
         return sum;
     }
 
     // printWholeBoard(B)
     // function that allows me to see the whole board
-    // runs at ^2 time so is used for testing and not for functionality
+    // runs at ^2 time so is used for testing smaller numbers and not for functionality
     static void printWholeBoard(int[][] B) {
         for(int i = 1; i <= B.length - 1; ++i){
             System.out.print("(");
@@ -56,7 +43,8 @@ class RQueens{
         }
     }
 
-    /*prints out a solution to n-queens in the format described*/
+    // printBoard(B)
+    // prints out a solution to n-queens if the user uses the -v option
     static void printBoard(int[][] B){
         System.out.print("(");
         for(int i = 1; i < B.length - 1; ++i){
@@ -65,7 +53,8 @@ class RQueens{
         System.out.println(B[B.length -1][0] + ")");
     }
 
-    /*n increments B[i][j] from its initial value of 0 to 1, and sets B[i][0] to j, thus indicating
+    // placeQueen(B,j,i)
+    /* n increments B[i][j] from its initial value of 0 to 1, and sets B[i][0] to j, thus indicating
     the existence of a queen on square (i,j). It will also decrement B[k][l] for every square (k, l) under attack
     from the new queen at (i,j), where i < k ≤ n and 1 ≤ l ≤ n*/
     static void placeQueen(int[][] B,int j, int i){
@@ -78,7 +67,8 @@ class RQueens{
         }
     }
 
-    /*decrements B[i][j] from 1 to 0, resets B[i][0] from j to 0, and increments B[k][l]
+    // removeQueen(B,j,i)
+    /* decrements B[i][j] from 1 to 0, resets B[i][0] from j to 0, and increments B[k][l]
     for every square (k, l) no longer under attack from the now absent queen at (i,j), where i < k ≤ n and
     1 ≤ l ≤ n*/
     static void removeQueen(int[][] B,int j, int i){
@@ -91,24 +81,21 @@ class RQueens{
         }
     }
 
+    // printErr()
+    // prints the error message and exits if incorrect input is given
     static void printErr(){
         System.err.println("Usage: RQueens [-v] number");
         System.err.println("Option: -v  verbose output, print all solutions");
         System.exit(0);
     }
 
-    /*read the command line arguments, then determine the value of n,
-    and what mode (normal or verbose) to run in. It will initialize an int array of size (n + 1) × (n + 1) to all
-    zeros, call function findSolutions() on this array in the correct mode, then print out the number of
-    solutions to n-queens that were found*/
     public static void main(String args[]){
+
+        // variables
         int n, sol, arglen = args.length;
         int[][] B;
 
-        //System.out.println(arglen);
-
-        //System.out.println(args[0]);
-
+        // if length of input is 2, check for "-v" and an integer, then print the solution as its solved
         if(arglen == 2) {
             if(!args[0].startsWith("-v")){
                 printErr();
@@ -121,6 +108,8 @@ class RQueens{
             } catch(NumberFormatException e1) {
                 printErr();
             }
+
+        // if length of input is 1, check for integer and solve.
         } else if(arglen == 1) {
             try{
                 n = Integer.parseInt(args[0]);
@@ -130,6 +119,8 @@ class RQueens{
             } catch (NumberFormatException e2) {
                 printErr();
             }
+        
+        // otherwise call the error message and exit
         } else {
             printErr();
         }
